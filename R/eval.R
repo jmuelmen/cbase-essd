9a811b0888d167fc655ac65ebc2d36ad70bd1047
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-02-15 18:06:51 +0100

    Evaluation functions skeleton

---
+++ R/eval.R
@@ -0,0 +1,17 @@
+#' Evaluation function (retrieval efficiency)
+#' 
+#' @param path Character.  Apply algorithm to all HDF files in this path
+#' @return A data frame containing cloud bases
+#' @export
+eval.efficiency <- function() {
+    
+}
+
+#' Evaluation function (METAR comparison)
+#' 
+#' @param path Character.  Apply algorithm to all HDF files in this path
+#' @return A data frame containing cloud bases
+#' @export
+eval.metar <- function() {
+    
+}
