8fddf27b77dfa93052deb452da1833779bf0e000
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-02-15 18:13:31 +0100

    Combination functions skeleton

---
+++ R/combine.R
@@ -0,0 +1,10 @@
+#' Combination of retrievals for comparison
+#'
+#' Combine METARs with C-BASE retrievals
+#' 
+#' @param path Character.  Apply algorithm to all HDF files in this path
+#' @return A data frame containing cloud bases
+#' @export
+combine.metar.cbase <- function() {
+    
+}
