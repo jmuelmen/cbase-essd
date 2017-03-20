95a62af4a98548f1237a982e58c0e3714313b614
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-03-20 12:35:10 +0100

    Finishing touches on bases.cbase

--- R/c-base.gen.R
+++ R/c-base.gen.R
@@ -16,7 +16,7 @@ bases.cbase <- function(path = "/projekt3/climate/DATA/SATELLITE/MULTI_SENSOR/DA
 
     sds <- hdf::h4list(lf[1])
 
-    doParallel::registerDoParallel(cores = 20)
+    doParallel::registerDoParallel(cores = 48)
     res <- plyr::adply(lf, 1, function(fname) {
         cal.datestring <- strsplit(basename(fname), "\\.")[[1]][2]
         cal.date <- as.POSIXlt(cal.datestring, format = "%Y-%m-%d", tz = "UTC") +
@@ -153,92 +153,9 @@ bases.cbase <- function(path = "/projekt3/climate/DATA/SATELLITE/MULTI_SENSOR/DA
         return(res)
     }, .parallel = TRUE)
 
-    saveRDS(res, out.name)
+    dplyr::select(res, -X1) %>%
+        saveRDS(out.name)
     return(res)
-
-    ## %>%
-    ##     ## ungroup() %>%
-    ##     ## ddply(~ time, function(x) {
-    ##     ##     with(x)
-    ##     as.data.frame() %>%
-    ##     select(-time) %>%
-    ##     filter(feature.above.surface == "clear air") %>%
-    ##     head(50)
-    
-    ## str()
-    
-    
-    ## label.lowest.cloud <- max(labels(f2[,1] == "cloud"))
-    ## feature.qa.lowest.cloud <- min(Feature_Type_QA[labels == ])
-    ## phase.lowest.cloud <- 
-    ## stack <- vertical.features.stack(f2[,1])
-    ## stack.idx.surface <- which(stack == "surface")
-
-    
-    ## labels <- label.vertical.features(Feature_Type[,1])
-    ## label.sfc <- labels[f2[,1] == "surface"]
-    ## label.lowest.cloud <- max(labels(f2[,1] == "cloud"))
-    ## feature.qa.lowest.cloud <- min(Feature_Type_QA[labels == ])
-    ## phase.lowest.cloud <- 
-    ## stack <- vertical.features.stack(f2[,1])
-    ## stack.idx.surface <- which(stack == "surface")
-    ## if (idx.surface > 1)
-    ##     feature.above.surface <- stack[stack.idx.surface - 1]
-    
-    ## height <- hdf::h4read(lf[1],sds,"CS_TRACK_Height")
-
-    ## doParallel::registerDoParallel(cores = 20)
-    ## res <- plyr::adply(lf, 1, function(fname) {
-    ##     print(fname)
-    ##     gc()
-
-    ##     out.fname <- paste("cloud-bases", gsub(".hdf", ".rds", basename(fname)), sep = "/")
-    ##     ## if (length(list.files("odran-bases", gsub(".hdf", ".rds", basename(fname)))) != 0)
-    ##     ##     return(readRDS(out.fname))
-        
-    ##     mask.cal <- hdf::h4read(fname,sds,"CALIPSO_Mask")
-    ##     darmask.liq <- hdf::h4read(fname,sds,"DARMASK_Liquid")
-    ##     calmask.cloud <- hdf::h4read(fname,sds,"CALIPSO_Mask")
-
-    ##     lat <- hdf::h4read(fname,sds,"CLOUDSAT_Latitude")
-    ##     lon <- hdf::h4read(fname,sds,"CLOUDSAT_Longitude")
-    ##     time <- hdf::h4read(fname,sds,"CLOUDSAT_UTC_Time")
-
-    ##     dardar.datestring <- strsplit(basename(fname), "_")[[1]][3]
-    ##     dardar.date <- as.POSIXlt(dardar.datestring, format = "%Y%j", tz = "UTC") ## strips off the %H%M%S --> 00:00:00 UTC
-
-    ##     dardar.time <- dardar.date + time
-
-    ##     ## Odran's way of getting the cloud base: liquid clouds, below
-    ##     ## 5 km, thin enough that CALIOP sees the underlying surface
-    ##     surf <- apply(mask.cal,2,function(x) {any(x==0)})
-    ##     cld <- apply(darmask.liq,2,function(x,y,z) {any(y[which(x==1)]<5)},y=height)
-    ##     idx.ok <- which(surf & cld)
-
-    ##     z.base <- array(NA,length(cld))
-    ##     z.base[idx.ok] <- sapply(idx.ok, function(x) tail(height[which(darmask.liq[,x]==1)],1))
-
-    ##     ## T.base <- array(NA,length(cld))
-    ##     ## T.base[idx.ok] <- sapply(idx.ok, function(x) tail(height[which(darmask.liq[,x]==1)],1))
-    ##     cal.cld <- apply(calmask.cloud,2,function(x,y,z) {any(y[which(x %in% 3)]<5)},y=height)
-    ##     cal.idx.ok <- which(surf & cal.cld)
-    ##     cal.med.cld <- apply(calmask.cloud,2,function(x,y,z) {any(y[which(x %in% 3:4)]<5)},y=height)
-    ##     cal.med.idx.ok <- which(surf & cal.med.cld)
-    ##     z.base.cal <- array(NA,length(cal.cld))
-    ##     z.base.cal[cal.idx.ok] <- sapply(cal.idx.ok, function(x) tail(height[which(calmask.cloud[,x] %in% 3)],1))
-    ##     z.base.cal.med <- array(NA,length(cal.med.cld))
-    ##     z.base.cal.med[cal.med.idx.ok] <- sapply(cal.med.idx.ok, function(x) tail(height[which(calmask.cloud[,x] %in% 3:4)],1))
-
-    ##     res <- dplyr::data_frame(dardar.time, lon = as.vector(lon), lat = as.vector(lat),
-    ##                              surf, cld, z.base, cal.cld, z.base.cal, cal.med.cld,
-    ##                              z.base.cal.med)
-    ##     ## str(res)
-    ##     saveRDS(res, file = out.fname)
-    ##     return(res)
-    ## }, .parallel = TRUE)
-    ## res <- dplyr::select(res, -X1)
-    ## saveRDS(res, out.name)
-    ## return(res)
 }
 
 #' @describeIn C-BASE_gen Bases by non-attenuated CALIOP in thin clouds (using DARDAR mask)
