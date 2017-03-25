618406953762dbd1690643d07a9079cd4d3f8fa5
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-03-25 11:32:34 +0100

    df.to.db tool

---
+++ R/tools.R
@@ -0,0 +1,15 @@
+#' Save a data.frame to an SQL database
+#'
+#' @param dd data.frame.
+#' @param fname character.  Output file name.
+#' @param name character.  Table name.
+#' @param indexes list.  List of variables to index.
+#' @return \code{dplyr::tbl} version of the database table
+#' @export
+df.to.db <- function(dd, fname, name, indexes = list("lon", "lat")) {
+    db <- dplyr::src_sqlite(fname, create = TRUE)
+    dplyr::copy_to(db, dd, name,
+            indexes, 
+            temporary = FALSE)
+    return(dplyr::tbl(db, name))
+}
