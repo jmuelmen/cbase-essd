210117e668d96866abceab20199decaec6173ebd
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-04-02 12:44:14 +0200

    Cleaned up duplication in eval.R and evalutils.R

--- R/evalutils.R
+++ R/evalutils.R
@@ -22,15 +22,16 @@ statistify <- function(df) {
             NA
         else res
     }
-    summarize(df,
-              n = n(),
-              cor = cor(ceilo, caliop, use = "na"),
-              cor.loc = cor(ceilo, caliop.local, use = "na"),
-              rmse = sqrt(mean((caliop * 1000 - ceilo) ^ 2, na.rm = TRUE)),
-              bias = mean(caliop * 1000 - ceilo, na.rm = TRUE),
-              slope = regress(ceilo, caliop, "slope"),
-              icpt = regress(ceilo, caliop, "icpt"),
-              rmse.fit = regress(ceilo, caliop, "rmse"))
+    df %>%
+        ## dplyr::filter(df, caliop < 2) %>%
+        dplyr::summarize(n = n(),
+                         cor = cor(ceilo, caliop, use = "na"),
+                         cor.loc = cor(ceilo, caliop.local, use = "na"),
+                         rmse = sqrt(mean((caliop * 1000 - ceilo) ^ 2, na.rm = TRUE)),
+                         bias = mean(caliop * 1000 - ceilo, na.rm = TRUE),
+                         slope = regress(ceilo, caliop, "slope"),
+                         icpt = regress(ceilo, caliop, "icpt"),
+                         rmse.fit = regress(ceilo, caliop, "rmse"))
 }
 
 #' Add summary statistics (linear regression, correlation, RMS error)
@@ -46,7 +47,7 @@ statistify <- function(df) {
 #' 
 statistify.df <- function(df) {
     stats <- statistify(df)
-    base.df <- select(stats, -(cor:rmse))
+    base.df <- dplyr::select(stats, -(cor:rmse))
     rbind(cbind(base.df, statistic = stats$n, statistic.name = "n"),
           cbind(base.df, statistic = stats$cor, statistic.name = "Correlation"),
           cbind(base.df, statistic = stats$cor.loc, statistic.name = "Local correlation"),
@@ -68,19 +69,20 @@ statistify.df <- function(df) {
 #' 
 regression_plot <- function(df, title, xlab = "50 km running mean CALIOP base (km)") {
     stats <- statistify(df)
-    gg <- ggplot(df, aes(caliop, ceilo)) +
-        geom_point(pch = ".") + stat_density2d(geom = "density2d") +
-        stat_smooth(method = "lm", col = "red", fill = "red", formula = y ~ x) +
-        stat_smooth(method = "loess", col = "blue", fill = "blue", formula = y ~ x) +
-        geom_abline(intercept = 0, slope = 1000, lty = "dashed", col = "lightgrey") +
-        coord_cartesian(ylim = c(-500,5500)) +
-        facet_grid(groups(df)) +
-        labs(title = title, x = xlab, y = "Ceilometer base (m)") +
-        geom_text(data = stats, aes(x = 5, y = 0,
+    gg <- ggplot2::ggplot(df, ggplot2::aes(caliop, ceilo)) +
+        ggplot2::geom_point(pch = ".") +
+        ggplot2::stat_density2d(geom = "density2d") +
+        ggplot2::stat_smooth(method = "lm", col = "red", fill = "red", formula = y ~ x) +
+        ggplot2::stat_smooth(method = "loess", col = "blue", fill = "blue", formula = y ~ x) +
+        ggplot2::geom_abline(intercept = 0, slope = 1000, lty = "dashed", col = "lightgrey") +
+        ggplot2::coord_cartesian(ylim = c(-500,5500)) +
+        ggplot2::facet_grid(dplyr::groups(df)) +
+        ggplot2::labs(title = title, x = xlab, y = "Ceilometer base (m)") +
+        ggplot2::geom_text(data = stats, ggplot2::aes(x = 5, y = 0,
                                     label = gsub("NA", "'NA'", sprintf("atop(atop(italic(n) == %.0f, italic(r)[loc] == %.2f~ italic(r)[fit] == %.2f), atop(italic(y) == %4.0f* x + %4.0f ~ fit ~ RMSE == %4.0f~m, RMSE == %4.0f~ m~bias == %4.0f~m))",
                                                     n, cor.loc, cor, slope, icpt, rmse.fit, rmse, bias))),
                   size = 4, vjust = 0, hjust = 1, parse = TRUE) +
-        theme_bw(base_size = 20)
+        ggplot2::theme_bw(base_size = 20)
     ## print(gg)
     return(list(stats = stats, ggplot = gg))
 }
