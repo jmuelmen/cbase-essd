d8fd7dbefcb44b763cba7ea571f73217e35715c6
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-04-11 10:17:40 +0200

    LeTeX number sanitizing function
    from xtables, in prep for the next commit

--- R/evalutils.R
+++ R/evalutils.R
@@ -92,3 +92,46 @@ regression_plot <- function(df, title, xlab = "50 km running mean CALIOP base (k
     ## print(gg)
     return(list(stats = stats, ggplot = gg))
 }
+
+sanitize.numbers <- function (str, type, math.style.negative = FALSE, math.style.exponents = FALSE) {
+    if (type == "latex") {
+        result <- str
+        if (math.style.negative) {
+            for (i in 1:length(str)) {
+                result[i] <- gsub("-", "$-$", result[i], fixed = TRUE)
+            }
+        }
+        if (is.logical(math.style.exponents) && !math.style.exponents) {
+        }
+        else if (is.logical(math.style.exponents) && math.style.exponents || 
+                 math.style.exponents == "$$") {
+            for (i in 1:length(str)) {
+                result[i] <- gsub("^\\$?(-?)\\$?([0-9.]+)[eE]\\$?(-?)\\+?\\$?0*(\\d+)$", 
+                                  "$\\1\\2 \\\\times 10^{\\3\\4}$", result[i])
+            }
+        }
+        else if (math.style.exponents == "ensuremath") {
+            for (i in 1:length(str)) {
+                result[i] <- gsub("([0-9.]+)[eE](-?)\\+?0*(\\d+)", 
+                                  "\\\\ensuremath{\\1 \\\\times 10^{\\2\\3}}", 
+                                  result[i])
+            }
+        }
+        else if (math.style.exponents == "UTF8" || math.style.exponents == 
+                 "UTF-8") {
+            for (i in 1:length(str)) {
+                if (all(grepl("^\\$?(-?)\\$?([0-9.]+)[eE]\\$?(-?)\\+?\\$?0*(\\d+)$", 
+                              result[i]))) {
+                    temp <- strsplit(result[i], "eE", result[i])
+                    result[i] <- paste0(temp[1], "×10", chartr("-1234567890", 
+                                                               "⁻¹²³⁴⁵⁴6⁴7⁴8⁴9⁰", temp[2]))
+                }
+            }
+        }
+        return(result)
+    }
+    else {
+        return(str)
+    }
+}
+
