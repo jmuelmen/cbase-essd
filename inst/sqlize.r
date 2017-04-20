fb2ecfb1c0ace577c74d409389704f9c5acecec0
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-04-20 16:53:54 +0200

    Make sqlite file for 2007 cloud bases

---
+++ inst/sqlize.r
@@ -0,0 +1,40 @@
+#! /usr/bin/Rscript --vanilla
+
+library(getopt)
+library(plyr)
+
+spec = matrix(c(
+##    'verbose', 'v', 2, "integer",
+    'help'              , 'h', 0, "logical",    "",
+    'input'             , 'i', 1, "character",  "",
+    'output'            , 'o', 1, "character",  "",
+    'table'             , 't', 1, "character",  "",
+    'indexes'           , 'x', 1, "character",  ""
+), byrow=TRUE, ncol=5);
+
+opt = getopt(spec, opt = commandArgs(TRUE));
+
+## if help was asked for print a friendly message
+## and exit with a non-zero error code
+if ( !is.null(opt$help) ) {
+    cat(getopt(spec, usage=TRUE));
+    q(status=1);
+}
+
+## get rid of spurious "ARGS" list element
+if (names(opt)[1] == "ARGS")
+    opt <- opt[-1];
+
+## parse arguments
+args <- llply(opt, function(text) {
+    eval(parse(text = text));
+});
+
+process <- function(input, output, table, indexes) {
+    df <- readRDS(input);
+    cbasetools::df.to.db(df, output, table, indexes);
+}
+
+ret <- do.call(process, args);
+q();
+
---
+++ inst/sqlize.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load r/3.2.0
+
+## RPATH=/pf/b/b380126/echam-ham/echam6.1-ham2.2-moz0.9/run
+RPATH=.
+
+echo "$*"
+
+Rscript --vanilla ${RPATH}/sqlize.r $*
