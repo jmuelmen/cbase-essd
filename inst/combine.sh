0a6ae7279fd2f68cbc305a37671937be2ca02639
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-04-21 17:58:12 +0200

    Export libraries that only exist on login nodes

--- inst/combine.sh
+++ inst/combine.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-module load r/3.2.0
+## R 3.3 depends on these shared libraries, but they do not exist on the compute nodes
+for i in libicuuc.so.42 libicui18n.so.42 libicudata.so.42 ; do
+    if ! [ -e $i ] ; then
+	cp  /usr/lib64/${i}.1 .
+	ln -s ${i}.1 $i
+    fi
+done
+
+module load r
 
 ## RPATH=/pf/b/b380126/echam-ham/echam6.1-ham2.2-moz0.9/run
-RPATH=.
+export RPATH=.
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD
 
 echo "$*"
 
