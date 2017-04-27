964883d91ba6e6ce1e831cba79bfbf378a0b37cb
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-04-27 12:06:23 +0200

    Add skeleton for C-BASE tuning functions

---
+++ R/c-base.tune.R
@@ -0,0 +1,14 @@
+#' C-BASE tuning functions
+#' 
+#' @param df Data.frame.  Contains tuning data set
+#' @return A list with two components, a list of tuned models for each
+#'     data category and a data.frame containing the index into said list of models 
+#' @name C-BASE_tune
+NULL
+
+#' @describeIn C-BASE_tune Tuning using linear models
+#' 
+#' @export
+tune.cbase.lm <- function(df) {
+    
+}
