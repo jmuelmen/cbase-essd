6626032ad81c4a9f2354b3b0b45dfd025252ba0d
Johannes Muelmenstaedt <johannes.muelmenstaedt@uni-leipzig.de>
2017-07-09 13:50:55 +0200

    Remove code not needed for SNOW

--- inst/cbase.r
+++ inst/cbase.r
@@ -4,24 +4,9 @@ library(getopt)
 library(cbasetools)
 library(plyr)
 
-combine.cbase.metar.dummy <-
-    function(eval = get.metar.2008(),
-             retrieval = dbtools::db_spec("cloud-bases-2008.sqlite", "cloudbase"),
-             resolution = resolution.min_cbh,
-             ncores = 72,
-             ...) {
-        str(eval);
-        print(retrieval);
-        print(resolution);
-        print(ncores);
-        return(resolution);
-    }
-
-
 spec <- matrix(c(
 ##    'verbose', 'v', 2, "integer",
     'help'              , 'h', 0, "logical",    "",
-    'ncores'            , 'n', 1, "integer",    "default: 72",
     'out.name'          , 'o', 1, "character",  "",
     'path'              , 'p', 1, "character",  "",
     'pattern'           , 'x', 1, "character",  "default: CAL_LID_L2_VFM-Standard-V40-10.*hdf"
