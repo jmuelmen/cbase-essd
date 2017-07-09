dacb26b62c6bc1dbe97b82efb53a23a6e4b1ced1
Johannes Muelmenstaedt <johannes.muelmenstaedt@uni-leipzig.de>
2017-07-09 13:24:01 +0200

    Wrap cbase.r in a SLURM script

---
+++ inst/cbase.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --account=bb1002
+#SBATCH --partition=compute2
+#SBATCH --job-name=cbase
+#SBATCH --nodes=9
+#SBATCH --ntasks-per-node=36
+#SBATCH --mem=250G
+#SBATCH --time=08:00:00
+#SBATCH --mail-type=FAIL
+
+
+
+# limit stacksize ... adjust to your programs need
+ulimit -s 102400
+
+# Environment settings to run a MPI parallel program
+# compiled with BullxMPI and Mellanox libraries
+# Load environment
+module load intel mxm fca bullxmpi_mlx
+module load r
+
+# Settings for Open MPI and MXM (MellanoX Messaging)
+# library
+export OMPI_MCA_pml=cm
+export OMPI_MCA_mtl=mxm
+export OMPI_MCA_mtl_mxm_np=0
+export MXM_RDMA_PORTS=mlx5_0:1
+export MXM_LOG_LEVEL=ERROR
+# Disable GHC algorithm for collective communication
+export OMPI_MCA_coll=^ghc
+
+# Use srun (not mpirun or mpiexec) command to launch
+# programs compiled with any MPI library
+mpirun -n 1 Rscript --vanilla ./cbase.r $*
