fd37859aaf956442b519e564fd4f24f90c8b8a04
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-07-22 17:27:35 +0200

    Add CBASE accessors (long and short form)
    long form collects RDS summaries from cbase.gen

--- R/cbase.R
+++ R/cbase.R
@@ -4,3 +4,29 @@ NULL
 
 #' @describeIn CBASE_accessors Get CBASE cloud heights
 #' @export
+get.cbase <- function() {
+    data(cbase, envir = environment())
+    cbase
+}
+
+#' @describeIn CBASE_accessors Get CBASE cloud heights
+#' @export
+get.cbase.bitsies <- function() {
+    lf.day <- list.files("~/cloud-bases", "CBASE-40.*D.rds", full.names = TRUE)
+    lf.night <- list.files("~/cloud-bases", "CBASE-40.*N.rds", full.names = TRUE)
+
+    doParallel::registerDoParallel(4)
+    df.day <- plyr::adply(lf.day, 1, function(fname) {
+        lf <- readRDS(fname)
+    }, .parallel = TRUE, .inform = TRUE)
+    df.night <- plyr::adply(lf.night, 1, function(fname) {
+        lf <- readRDS(fname)
+    }, .parallel = TRUE, .inform = TRUE)
+
+    cbase <- dplyr::bind_rows(dplyr::mutate(df.day, daynight = "day"),
+                              dplyr::mutate(df.night, daynight = "night")) %>%
+        dplyr::select(segment, X1 : n, pred.ceilo, pred.rmse, daynight)
+    
+    save(cbase, file = "~/r-packages/cbasetools/data/cbase.rda")
+    cbase
+}
--- R/data.R
+++ R/data.R
@@ -35,3 +35,21 @@
 #' }
 #' @source See the first example in \code{\link{get.metars.from.archive}}
 "metar.2008"
+
+#' CBASE retrievals (2007 and 2008)
+#'
+#' A dataset containing the CBASE cloud base data
+#'
+#' @format A data frame with \eqn{n} variables:
+#' \describe{
+#'   \item{lon}{longitude; -180 < lon < 180}
+#'   \item{lat}{latitude}
+#'   \item{denom}{denominator for fractions (character):
+#'     \describe{
+#'        \item{all}{number of satellite overpasses}
+#'        \item{cloud}{number of cloudy satellite overpasses}
+#'        \item{rain}{number of raining satellite overpasses}
+#'     }}
+#' }
+#' @source \code{\link{get.cbase.bitsies}}
+"cbase"
