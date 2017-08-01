017167739da5c49f0b94c49056e6272f77b10618
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-08-01 14:04:14 +0200

    Add vis.n() (number of retrievals per grid box)

--- R/visutils.R
+++ R/visutils.R
@@ -98,3 +98,51 @@ vis.cdf <- function(df) {
         ggplot2::facet_grid(daynight ~ rmse) +
         ggplot2::theme_bw(18) 
 }
+
+#' Visualization of geographical distribution of cloud-base
+#' uncertainties (CDF)
+#' 
+#' @param df data.frame.  Result of \code{bases.cbase()}
+#' @return NULL
+#' @export
+vis.n <- function(df, base_size) {
+    doParallel::registerDoParallel(4)
+    df %>%
+        plotutils::discretize(lon, seq(-180, 180, 2)) %>%
+        plotutils::discretize(lat, seq(-90, 90, 2)) %>%
+        dplyr::group_by(lon, lat, daynight) %>%
+        dplyr::summarize(n = n()) %>%
+        ggplot2::ggplot(ggplot2::aes(x = lon, y = lat, fill = n)) +
+        ggplot2::geom_raster() +
+        plotutils::geom_world_polygon() +
+        plotutils::scale_x_geo(facet = TRUE) +
+        plotutils::scale_y_geo() +
+        ggplot2::scale_fill_distiller(palette = "Spectral", direction = -1) +
+        ggplot2::facet_grid(. ~ daynight) +
+        ggplot2::theme_bw(18) 
+
+    df %>%
+        plotutils::discretize(lon, seq(-180, 180, 5)) %>%
+        plotutils::discretize(lat, seq(-90, 90, 5)) %>%
+        plyr::ddply(~ lon + lat + daynight, function(df) {
+            cdf <- ecdf(df$pred.rmse)
+            data.frame(rmse = seq(320,480, by = 40)) %>%
+                dplyr::mutate(cdf = cdf(rmse),
+                              ncdf = nrow(df) * cdf)
+        }, .parallel = TRUE) -> tmp
+
+    tmp %>%
+        plotutils::discretize(cdf, c(0,0.05,0.1,0.15,0.2,0.4,0.6,0.8,1)) %>%
+        plotutils::discretize(ncdf, c(0,10,50,100,200,500,1000)) %>%
+        dplyr::mutate(cdf = factor(cdf),
+                      ncdf = factor(ncdf)) %>%
+        ggplot2::ggplot(ggplot2::aes(x = lon, y = lat, fill = ncdf)) +
+        ggplot2::geom_raster() +
+        plotutils::geom_world_polygon() +
+        plotutils::scale_x_geo(facet = TRUE) +
+        plotutils::scale_y_geo() +
+        ggplot2::coord_fixed(1) +
+        ggplot2::scale_fill_brewer(palette = "Reds", direction = +1) +
+        ggplot2::facet_grid(rmse ~ daynight) +
+        ggplot2::theme_bw(base_size) + ggplot2::theme(legend.position = "bottom")
+}
