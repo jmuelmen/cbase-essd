ef8befa6850a956b9fab7fc462a013899c5f4a07
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2017-08-10 07:50:36 +0200

    Combine 40 and 100 km cbase.rda; add MSL CBH

--- R/cbase.R
+++ R/cbase.R
@@ -12,22 +12,27 @@ get.cbase <- function() {
 #' @describeIn CBASE_accessors Get CBASE cloud heights
 #' @export
 get.cbase.bitsies <- function() {
-    lf.day <- list.files("~/cloud-bases", "CBASE-40.*D.rds", full.names = TRUE)
-    lf.night <- list.files("~/cloud-bases", "CBASE-40.*N.rds", full.names = TRUE)
-
-    doParallel::registerDoParallel(4)
-    df.day <- plyr::adply(lf.day, 1, function(fname) {
-        lf <- readRDS(fname)
-    }, .parallel = TRUE, .inform = TRUE)
-    df.night <- plyr::adply(lf.night, 1, function(fname) {
-        lf <- readRDS(fname)
-    }, .parallel = TRUE, .inform = TRUE)
-
-    cbase <- dplyr::bind_rows(dplyr::mutate(df.day, daynight = "day"),
-                              dplyr::mutate(df.night, daynight = "night")) %>%
-        dplyr::select(segment, time : n, pred.ceilo, pred.rmse, daynight) %>%
-        dplyr::mutate(daynight = factor(daynight))
+    cbase <- plyr::adply(c(40, 100), 1, function(dist) {
+        lf.day <- list.files("~/cloud-bases", sprintf("CBASE-%d.*D.rds", dist),
+                             full.names = TRUE)
+        lf.night <- list.files("~/cloud-bases", sprintf("CBASE-%d.*N.rds", dist),
+                               full.names = TRUE)
+        doParallel::registerDoParallel(4)
+        df.day <- plyr::adply(lf.day, 1, function(fname) {
+            lf <- readRDS(fname)
+        }, .parallel = TRUE, .inform = TRUE)
+        df.night <- plyr::adply(lf.night, 1, function(fname) {
+            lf <- readRDS(fname)
+        }, .parallel = TRUE, .inform = TRUE)
+        
+        cbase <- dplyr::bind_rows(dplyr::mutate(df.day, daynight = "day"),
+                                  dplyr::mutate(df.night, daynight = "night")) %>%
+            dplyr::select(segment, time : n,
+                          pred.ceilo, pred.ceilo.msl, pred.rmse, daynight) %>%
+            dplyr::mutate(daynight = factor(daynight),
+                          dist = as.integer(dist))
+    }, .id = NULL)
     
-    save(cbase, file = "~/r-packages/cbasetools/data/cbase.rda")
+    save(cbase, file = "~/r-packages/cbasetools/data/cbase.rda", compress = "xz")
     cbase
 }
