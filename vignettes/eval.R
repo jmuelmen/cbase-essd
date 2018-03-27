aaa10181365a22b377c798d76733a40fb13016cc
Johannes Mülmenstädt <johannes.muelmenstaedt@uni-leipzig.de>
2018-03-27 16:10:52 +0200

    Switch ASOS plot to a more pleasing color scheme

--- vignettes/eval.R
+++ vignettes/eval.R
@@ -331,7 +331,7 @@ ggplot(df.asos, aes(x = lon, y = lat, col = min.dist, size = n)) +
     borders("state") +
     scale_size("$N$", trans = "identity", range = c(0.05, 2),
                guide = ggplot2::guide_legend(title.vjust = 0.5)) +
-    scale_color_distiller("$\\min(D)$ (km)", palette = "Oranges",
+    scale_color_distiller("$\\min(D)$ (km)", palette = "Blues",
                           guide = ggplot2::guide_colorbar(title.vjust = 0.75)) +
     labs(x = NULL, y = NULL) +
     scale_x_continuous(breaks = NULL) + scale_y_continuous(breaks = NULL) +
